7e8902a2649c6541b919d748a10f8ed3cca6f57b
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-08-27 16:40:59 -0300

    add-install-sh-coments

--- SMH-UI/src/app/enums/repository-api.enum.ts
+++ SMH-UI/src/app/enums/repository-api.enum.ts
@@ -1,6 +1,6 @@
 export enum RepositoryApi {
 
     geoserverTerraMaLocal = 'http://www.terrama2.dpi.inpe.br/chuva/geoserver/wms?',
-    smh_api = 'http://150.163.17.142:5000'
-
+    smh_api = 'http://localhost:5000'
+    // smh_api = 'http://150.163.17.142:5000'
 }
--- start.sh
+++ start.sh
@@ -1,9 +1,39 @@
+echo " => Criando a imagem api-flask 1/5
+    
+    "
 cd api-flask/
 docker build -t api-flask:latest .
+echo "
+ => Executando o container flask com a imagem gerada api-flask 2/5
+    
+    "
 docker container run --name flask -d -p 5000:5000 api-flask:latest
+echo "
+
+ => Criando o projeto angular executável SMH-UI (build) 3/5
+    
+    "
 cd ../SMH-UI
 npm install
 ng build
+echo "
+
+ => Criando uma imagem com o projeto executável SMH-UI 4/5
+    
+    "
 cp Dockerfile dist/SMH-UI
 docker build -t smh-ui:latest dist/SMH-UI
+echo "
+
+ => Executando o container com a imagem gerada SMH-UI 5/5
+    
+    "
 docker container run --name app-smh-ui -d -p 8082:8080 smh-ui:latest
+echo "
+
+ Finalizando OK ;)
+ 
+    API-FLASK => http://0.0.0.0:5000
+    SMH-UI => http://0.0.0.0:8082
+
+    "
