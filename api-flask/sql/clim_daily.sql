706f119ee56316bb50a674e964a6a3f0f03694fb
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-10-23 11:46:01 -0300

    colors-fixed

--- api-flask/sql/clim_daily.sql
+++ api-flask/sql/clim_daily.sql
@@ -1,4 +1,4 @@
-SELECT municipio.nome1 as nome_municipio, clim_daily.maxima, clim_daily.media, clim_daily.dia, clim_daily.mes, clim_daily.execution_date
+SELECT municipio.nome1 AS nome_municipio, clim_daily.maxima, clim_daily.media, clim_daily.dia, clim_daily.mes, clim_daily.execution_date
 FROM public.municipios_brasil municipio, public.an_municipio_clim_daily clim_daily
 WHERE municipio.geocodigo = '{geocodigo}'
 AND clim_daily.mes = '{mes}'
--- api-flask/sql/clim_monthly.sql
+++ api-flask/sql/clim_monthly.sql
@@ -1,4 +1,4 @@
-SELECT municipio.nome1, clim_monthly.maxima, clim_monthly.media, clim_monthly.mes
+SELECT municipio.nome1 AS nome_municipio, clim_monthly.maxima, clim_monthly.media, clim_monthly.mes
 FROM public.municipios_brasil municipio, public.an_municipio_clim_monthly clim_monthly
 WHERE municipio.geocodigo = '{geocodigo}' AND mes = '{mes}' AND municipio.fid = clim_monthly.fid
 ORDER BY execution_date;
