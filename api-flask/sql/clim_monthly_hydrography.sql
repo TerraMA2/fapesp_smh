e5148c7149d615c01e5f085112d9e1b31479b4c6
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2020-01-20 16:14:30 -0300

    fixed-atribute-error

--- api-flask/sql/clim_monthly_hydrography.sql
+++ api-flask/sql/clim_monthly_hydrography.sql
@@ -2,5 +2,5 @@ SELECT bacias.ps1_nm AS nome_bacia, clim_monthly.maxima, clim_monthly.media, TRI
 FROM public.geoft_pnrh_sub1 bacias, public.an_bacias_clim_monthly clim_monthly
 WHERE bacias.ps1_cd = '{codigo}'
 AND UPPER(TRIM(TO_CHAR(clim_monthly.execution_date, 'Month'))) = UPPER('{mes}')
-AND bacias.gid = clim_monthly.fid
+AND bacias.gid = clim_monthly.gid
 ORDER BY clim_monthly.execution_date;
