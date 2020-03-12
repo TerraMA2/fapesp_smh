1a067ca64e3c63924b6c5e41a7b0eca32721e3b7
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2020-03-12 14:54:32 -0300

    white spaces

--- api-flask/sql/cities_by_state.sql
+++ api-flask/sql/cities_by_state.sql
@@ -1,1 +1,3 @@
-SELECT nome1 AS nome_municipio, geocodigo, latitude, longitude FROM public.municipios_brasil WHERE uf = '{uf}' ORDER BY nome1;
+SELECT nome1 AS nome_municipio, geocodigo, latitude, longitude
+FROM public.municipios_brasil
+WHERE uf = '{uf}' ORDER BY nome1;
